521e6ab9a5db04e83762ecd4d2598b57a3093bef
Trinh Tuan <trinhtuan2801@gmail.com>
2024-08-02 02:11:34 +0700

    done 135-candy

---
+++ scripts/135-candy.js
@@ -0,0 +1,40 @@
+function candy(ratings) {
+  let L = 1;
+  let R = 1;
+  let sum = 0;
+  const peaks = [];
+  let sharedValeys = 0;
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      if (R > 1) {
+        peaks.push([L, R]);
+        L = 1;
+        R = 1;
+        sharedValeys++;
+      }
+      L++;
+    } else if (ratings[i] < ratings[i - 1]) {
+      R++;
+    } else {
+      peaks.push([L, R]);
+      L = 1;
+      R = 1;
+    }
+  }
+  peaks.push([L, R]);
+//   console.log(peaks, sharedValeys);
+  const sigma = (start, end) => {
+    if (end === 0) return 0;
+    return ((end + start) * (end - start + 1)) / 2;
+  };
+  for (const [L, R] of peaks) {
+    sum += sigma(1, L - 1) + sigma(1, R - 1) + Math.max(L, R);
+  }
+
+  sum -= sharedValeys;
+
+  return sum;
+}
+
+const ratings = [1, 2, 87, 87, 2, 1];
+console.log(candy(ratings));
